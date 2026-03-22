b3a007a5bc9cf65ad50a12f9586d39f4a7498971
Nikita Kuznetsov <149796739+v3xtr@users.noreply.github.com>
2026-03-22 18:19:39 +0100

    Delete auth-service directory

--- auth-service/src/main/java/com/youtube/auth_service/pkg/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.youtube.auth_service.pkg;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- auth-service/src/main/resources/db/migrations/V1__Create_Users_Table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password TEXT NOT NULL
-);
-
-CREATE INDEX IF NOT EXISTS idx_users_email ON users(email);
