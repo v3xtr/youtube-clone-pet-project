19414af5f28766c3e5b230a301757d9b35290744
v3xtr <j75yhciamoc@gmail.com>
2026-03-22 14:32:01 +0100

    removed password from broker

--- auth-service/src/main/java/com/youtube/auth_service/delivery/broker/AuthBroker.java
+++ auth-service/src/main/java/com/youtube/auth_service/delivery/broker/AuthBroker.java
@@ -1,10 +1,15 @@
 package com.youtube.auth_service.delivery.broker;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
 
 import com.youtube.auth_service.delivery.broker.port.IAuthBroker;
 import com.youtube.auth_service.internal.domain.entities.UserEntity;
+import com.youtube.auth_service.internal.domain.entities.UserRegisteredEventEntity;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,7 +28,15 @@ public class AuthBroker implements IAuthBroker{
         }
 
         try {
-            boolean sent = streamBridge.send("userRegistered-out-0", userEntity);
+
+            UserRegisteredEventEntity event = UserRegisteredEventEntity.builder()
+                .eventId(UUID.randomUUID())
+                .userId(userEntity.getId())
+                .email(userEntity.getEmail())
+                .createdAt(LocalDateTime.now())
+                .build();
+
+            boolean sent = streamBridge.send("userRegistered-out-0", event);
 
             if (sent){
                 log.info("[AuthBroker]: User {} published successfully to Kafka", userEntity.getId());
